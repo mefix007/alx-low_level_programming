d78f9e2bba2617795826bcd212047c80f3aa0039
mefix007 <femdus007@gmail.com>
2022-09-13 13:20:33 -0700

    0-positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,15 +19,15 @@ l = n % 10;
 
 if (l > 5)
 {
-	printf("%d is positive\n", n, l);
+	printf("Last digit of %d is %d and is greater than 5\n", n, l);
 }
 else if (l == 0)
 {
-	printf("%d is zero\n", n, l);
+	printf("Last digit of %d is %d and is 0\n", n, l);
 }
 else
 {
-	printf("%d is negative\n", n, l);
+	printf("Last digit of %d is %d and is less than 0\n", n, l);
 }
 return (0);
 }
