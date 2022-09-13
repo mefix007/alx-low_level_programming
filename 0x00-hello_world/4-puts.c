5d71337c1184a7a91bf47ce7f8cd9cb7f04669a4
mefix007 <femdus007@gmail.com>
2022-09-12 22:32:39 -0700

    4 puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - main puts
- * Return: 0
+ * main - Enter puts
+ *
+ * Return: Always 0 (Successful)
  */
 int main(void)
-{ 
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+{
+ puts("\"programming is like building a multilingual puzzle");
+    return (0);
 }
