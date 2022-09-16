ac406ed0cff7b6218f3c5ca408df26cd01cf21f2
mefix007 <femdus007@gmail.com>
2022-09-16 07:02:17 -0700

    2 largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,22 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-    
-int largest;
-if (a > b && a > c)
-{
-largest = a;
-}
+    int largest;
 
-else if (b > c)
-{
-largest = b;
-}
+    if (a > b && a > c)
+    {
+        largest = a;
+    }
+    else if (b > a && b > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
 
-else
-{
-largest = c;
-}
-
-return (largest);
+    return (largest);
 }
