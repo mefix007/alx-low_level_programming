be7f614a13735a95f4f585379eea7fd0622ba5a1
mefix007 <femdus007@gmail.com>
2022-09-21 14:52:45 -0700

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,27 +4,24 @@
 * rev_string - this reserve a string
 * @s: pointer
 */
-
-
 void rev_string(char *s)
 {
 	int len = 0, index = 0;
 	char tmp;
-	
+
+
+
 	while (s[index++])
-	
 		len++;
-	
+
+
+
 	for (index = len - 1; index >= len / 2; index--)
-	
 	{
 		tmp = s[index];
-	
 		s[index] = s[len - index - 1];
-	
 		s[len - index - 1] = tmp;
-	
 	}
-
-
 }
+
+
