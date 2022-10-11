e6ea862cb0586496356c6ba58d7dc12a0a3ba3f4
mefix007 <femdus007@gmail.com>
2022-10-10 20:48:30 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,13 +15,14 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	m = malloc((max - min +1) * sizeof(int));
+	m = malloc((max - min + 1) * sizeof(int));
 
 	if (m == 0)
 		return (NULL);
 
-	for (i = 0; min + i<= max; i++)
+	for (i = 0; min + i <= max; i++)
 		m[i] = min + i;
-	
-	return (a)
+	{
+		return (a);
+	}
 }
